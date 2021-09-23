84532d5fcf066dd618cefbe0632ce8f3baa61dad
Freshtuna <jun17114@gmail.com>
2021-09-23 23:17:23 +0900

    Result footer 개선

--- src/components/ResultFooter.jsx
+++ src/components/ResultFooter.jsx
@@ -3,15 +3,34 @@ import styled from "styled-components";
 function ResultFooter () {
 
     return (
-        <>
+        <Wrapper>
             <SymbolImage src={"/images/symbol_blue@4x.png"} />
-        </>
+            <PresentBy src={"/images/footer_text@3x.png"} />
+            <Contributors src={"/images/contributors@3x.png"} />
+        </Wrapper>
     )
 }
 
 const SymbolImage = styled.img`
     width: 70px;
     margin-bottom: 24px;
+`;
+
+const PresentBy = styled.img`
+    width: 85px;
+    margin-bottom: 5px;
+`;
+
+const Contributors = styled.img`
+    margin-top: 5px;
+    width: 185px;
+`;
+
+const Wrapper = styled.section`
+    margin-bottom: 135px;
+    display:flex;
+    flex-direction: column;
+    align-items: center;
 `;
 
 export default ResultFooter;
