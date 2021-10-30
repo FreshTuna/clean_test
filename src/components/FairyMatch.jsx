325ab71d0c80818b8f1ba2b13e7ac766ebdebe74
Freshtuna <jun17114@gmail.com>
2021-10-30 15:05:30 +0900

    궁함 result로 이동

--- src/components/FairyMatch.jsx
+++ src/components/FairyMatch.jsx
@@ -1,24 +1,29 @@
 import {useEffect} from "react";
 import styled from "styled-components";
 import {MBTI_COMBINATION, MBTI_NUMBER} from "../common/constant";
+import {useHistory} from "react-router-dom";
 
 function FairyMatch(props) {
+    const history = useHistory();
     useEffect( () => {
-
     }, []);
+
+    const matchingFairy = (mbti) => {
+        history.push(`/result/${mbti}`);
+    }
 
     return (
         <Wrapper>
             <MatchContainer>
                 <GoodFairy src={"/images/good_mate@3x.png"} />
-                <FairyFrame>
+                <FairyFrame onClick={ () => matchingFairy(props.fit)}>
                     <FairyFrameImage src={`https://cleanhome-dev.s3.ap-northeast-2.amazonaws.com/mbti/match/${MBTI_COMBINATION[props.fit].NAME}_frame_fit%403x.png`} />
                 </FairyFrame>
                 <FairyName src={`https://cleanhome-dev.s3.ap-northeast-2.amazonaws.com/mbti/title/${MBTI_COMBINATION[props.fit].NAME}_title%403x.png`} />
             </MatchContainer>
             <MatchContainer>
                 <BadFairy src={"/images/bad_mate@3x.png"} />
-                <FairyFrame>
+                <FairyFrame onClick={ () => matchingFairy(props.unfit)}>
                     <FairyFrameImage src={`https://cleanhome-dev.s3.ap-northeast-2.amazonaws.com/mbti/match/${MBTI_COMBINATION[props.unfit].NAME}_frame_unfit%403x.png`} />
                 </FairyFrame>
                 <FairyName src={`https://cleanhome-dev.s3.ap-northeast-2.amazonaws.com/mbti/title/${MBTI_COMBINATION[props.unfit].NAME}_title%403x.png`} />
