bc38eb0e961b5c45bc779a93a8d2483c02b55020
Freshtuna <jun17114@gmail.com>
2021-10-30 13:59:40 +0900

    이미지 속도 테스트

--- src/components/AnswerWithImage.jsx
+++ src/components/AnswerWithImage.jsx
@@ -7,7 +7,12 @@ function AnswerWithImage(props) {
     return (
         <Wrapper>
                 <>
-                    <QuestionImage src={`/images/questions/question_${props.questionNumber}_image@3x.png`} />
+                    {
+                        props.questionNumber < 5 ?
+                            <QuestionImage src={`https://cleanhome-dev.s3.ap-northeast-2.amazonaws.com/mbti/questions/question_${props.questionNumber}_image%403x.png`} />
+                            :
+                            <QuestionImage src={`/images/questions/question_${props.questionNumber}_image@3x.png`} />
+                    }
                     <AnswerImage src={`/images/answers/answer_${props.questionNumber}_up@3x.png`} onClick={() => props.answerHandler(1)}/>
                     <div style={{marginBottom:"12px"}} />
                     <AnswerImage src={`/images/answers/answer_${props.questionNumber}_down@3x.png`} onClick={() => props.answerHandler(-1)} />
